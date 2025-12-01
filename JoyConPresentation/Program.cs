997567746a999a76530f3d646754db49fdd71029
ramune314159265 <ramune314159265@gmail.com>
2025-12-02 01:00:29 +0900

    プロジェクト ファイルを追加します。

---
+++ JoyConPresentation/Program.cs
@@ -0,0 +1,21 @@
+﻿using HidSharp;
+
+class Program
+{
+    static void Main()
+    {
+        HidDevice? device = GetDevice();
+        if(device == null)
+        {
+            Console.WriteLine("JoyCon が見つかりませんでした");
+            return;
+        }
+    }
+
+    static HidDevice? GetDevice()
+    {
+        DeviceList list = DeviceList.Local;
+        var nintendos = list.GetHidDevices(0x057e);
+        return nintendos.FirstOrDefault();
+    }
+}
